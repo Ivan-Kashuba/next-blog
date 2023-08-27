03b3f2777fb98ad8fe6dbad807e366d19d8e34f4
Ivan Kashuba <kashubaivan7@gmail.com>
2023-08-27 22:39:14 +0300

    initial commit

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,7 +8,7 @@ export interface Post {
     body: string;
 }
 async function getData() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/postsa', {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
         next: {
             revalidate: 60,
         },
